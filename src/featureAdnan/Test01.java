5c6c91d81facce16a8b86ccd11dce1570f91c629
Adnan Sen <senadnan82@gmail.com>
2022-07-31 21:57:08 +0300

    new codes added

--- src/featureAdnan/Test01.java
+++ src/featureAdnan/Test01.java
@@ -1,4 +1,10 @@
 package featureAdnan;
 
 public class Test01 {
-}
+
+    public static void main(String[] args) {
+        System.out.println("Hello guys.. Welcome to our new GitHub Project");
+    }
+
+    }
+
