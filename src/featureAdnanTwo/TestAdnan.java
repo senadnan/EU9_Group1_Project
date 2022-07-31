46de306cf03b4587c63104e3c4b2737a671f7898
Adnan Sen <senadnan82@gmail.com>
2022-07-31 22:17:13 +0300

    new class added

---
+++ src/featureAdnanTwo/TestAdnan.java
@@ -0,0 +1,11 @@
+package featureAdnanTwo;
+
+public class TestAdnan {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("I added some good features to our project");
+    }
+
+}
