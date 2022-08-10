07ff85ba96a3ed428a008eac3a869feabbcdae79
refikturk <109914704+refikturk@users.noreply.github.com>
2022-08-10 19:38:06 +0300

    Delete src/featureRefik directory

--- src/featureRefik/TestCase001.java
+++
@@ -1,9 +0,0 @@
-package featureRefik;
-
-public class TestCase001 {
-
-    public static void main(String[] args) {
-
-        System.out.println("Hello Dear Group-1, I have made some good updates on the project");
-    }
-}
