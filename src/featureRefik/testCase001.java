b6ebd2d94f536786e35e3be95bc0bcf6eec7cc63
refikturk <rfkturk43@gmail.com>
2022-08-10 19:36:10 +0300

    Created Test Case 001

---
+++ src/featureRefik/testCase001.java
@@ -0,0 +1,10 @@
+package featureRefik;
+
+public class testCase001 {
+
+    public static void main(String[] args) {
+
+        System.out.println("I have made some changes on project");
+
+    }
+}
